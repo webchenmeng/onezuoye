8a9baa235ef57484f006d74896ef28b835ddc7a3
chenmeng <877511163@qq.com>
2016-12-05 10:45:36 +0800

    改1

--- js/js.js
+++ js/js.js
@@ -1,38 +1,49 @@
-var m=angular.module("myApp",[]);
-		m.controller("app",["$scope","$filter",function($scope,$filter){
-			var arr=[
-				{"FirstName":"FANG",
+angular.module("myApp",[])
+
+.controller("myController",["$scope",function($scope){
+     var userInfo = [
+        {"FirstName":"FANG",
 				"LastName":"vane",
 				"Gender":"Male",
 				"Salary":"12,333.50",
 				"Salary1":"¥12,333.5",
-				"Birthday":"2017-07-11"},
+				"Birthday":new Date("2017-12-25")},
 				{"FirstName":"SARA",
 				"LastName":"rose",
 				"Gender":"Female",
 				"Salary":"23,334.23",
 				"Salary1":"¥23,334.2",
-				"Birthday":"1997-02-03"},
+				"Birthday":new Date("1335-12-25")},
 				{"FirstName":"AAM",
 				"LastName":"hot",
 				"Gender":"Male",
 				"Salary":"66,880.50",
 				"Salary1":"¥68,880.5",
-				"Birthday":"1986-03-04"},
+				"Birthday":new Date("1986-12-25")},
 				{"FirstName":"MARK",
 				"LastName":"bear",
 				"Gender":"Male",
 				"Salary":"68,000.50",
 				"Salary1":"¥66,880.5",
-				"Birthday":"1968-03-22"}
+				"Birthday":new Date("1995-12-25")}
 				
-			];
-			 $scope.arr1=arr;
-		
-			var isopen=true;
-			$scope.sort=function(str){
-				$scope.arr1=$filter("orderBy")($scope.arr1,str,isopen);
-				isopen=!isopen;
-			};
+     ]
+     $scope.userInfo = userInfo;
+     $scope.sortColumn = "name";   // 排序字段
+     $scope.reverseSort = false;   // 默认的排序方式
 
-		}]);
+     // 设置排序字段
+     $scope.setSort = function(column){
+         // 如果传递过来的排序字段和当前字段一致，则要降序，否则升序
+         $scope.reverseSort = $scope.sortColumn == column?!$scope.reverseSort:false;
+         $scope.sortColumn = column;
+     }
+
+
+     // 设置类
+     $scope.getClass = function(column){
+         if($scope.sortColumn == column){
+             return $scope.reverseSort? "allow-down" : "allow-up";
+         }
+     }
+}])
